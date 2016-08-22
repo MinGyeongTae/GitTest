db1e3d8c745741b5e60d4e7e557f718ef5574e94
민경태 <krivnon@konkuk.ac.kr>
2016-08-22 17:28:37 +0900

    안되서 좀 바

--- GitC++테스트/Test.cpp
+++ GitC++테스트/Test.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 
 using namespace std;
-int main()
-{
-	cout << "hi요" << endl;
-	cout << "잘 안되니"<<endl;
-	cout << "이거 되요?";
-	msgThank();
-	getchar();
-}
+
 
 void msgThank() {
 
 	cout << "msgThank";
+}
+
+int main()
+{
+	cout << "hi요" << endl;
+	cout << "잘 안되니"<<endl;
+	cout << "이거 되요?"<<endl;
+	msgThank();
+	getchar();
 }
